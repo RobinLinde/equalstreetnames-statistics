8e64f649fff615b471544fc51a4352a8dfb5183f
Robin van der Linde <robin.vanderlinde@gmail.com>
2023-12-30 00:51:16 +0100

    💄 Change styling

--- src/routes/[country]/[city]/+page.svelte
+++ src/routes/[country]/[city]/+page.svelte
@@ -9,25 +9,30 @@
 	const compiledStats: CompiledStatistics = data.statistics;
 </script>
 
-<div class="content">
-	<div class="graph">
-		<Graph data={compiledStats} />
-	</div>
-	<div class="piegraph">
-		<PieGraph data={compiledStats} />
+<div class="wrapper">
+	<div class="content">
+		<div class="graph">
+			<Graph data={compiledStats} />
+		</div>
+		<div class="piegraph">
+			<PieGraph data={compiledStats} />
+		</div>
 	</div>
 </div>
 
 <style lang="postcss">
+	.wrapper {
+		@apply w-full h-auto;
+	}
 	.content {
-		@apply flex flex-row;
+		@apply flex flex-row w-full h-full;
 	}
 
 	.graph {
-		@apply w-full h-auto;
+		@apply w-1/2 h-full;
 	}
 
 	.piegraph {
-		@apply w-full h-auto;
+		@apply w-1/2 h-1/2;
 	}
 </style>
